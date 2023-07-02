a7e426c9e15f09f8b21a19e07beeebd76f719942
Collins07 <collinsabaya07@gmail.com>
2023-07-03 00:03:44 +0300

    remove editing option

--- reforestation/forests/urls.py
+++ reforestation/forests/urls.py
@@ -5,8 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('' ,views.forest, name='forest'),
     path('add_forest' ,views.add_forest, name='add_forest'),
-    path('edit_forest/<int:id>' ,views.forest_edit, name='forest-edit'),
-    path('delete_forest/<int:id>' ,views.forest_delete, name='forest-delete'),
      path('search-forest' , csrf_exempt(views.search_forest), name='search_forest'),
      path ('export_csv', views.export_csv, name='export-csv'),
      path ('export_pdf', views.export_pdf, name='export-pdf'),
